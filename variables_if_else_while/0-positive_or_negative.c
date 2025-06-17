4a101695b076bc9835064d4593f26f06207b9339
root <decourttthomas@gmail.com>
2025-06-17 12:43:34 +0000

    Add task 1 avec if

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,5 +13,12 @@ int main(void)
 int n;
 srand (time(0)); 
 n = rand() - RAND_MAX / 2;
+ if (n > 0)
+   printf("is positive")
+   else if (n = 0)
+   printf("is zero")
+   else (n < 0)
+   printf ("is negative")
+	 
 return (0);
 }
