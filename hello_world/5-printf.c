94a75d0ed05de58f9c59cd2d6c5549fbaa59e764
root <decourttthomas@gmail.com>
2025-06-17 09:56:02 +0000

    Add tas 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
