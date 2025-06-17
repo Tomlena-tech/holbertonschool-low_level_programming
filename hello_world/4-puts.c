5e7bdd42ed4b63d1eef4a3d5f096c8b8bf675cdd
root <decourttthomas@gmail.com>
2025-06-17 09:08:51 +0000

    Add 4tasks

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints "Programming is like building a multilingual puzzle"
+ * using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
