173efc28166d286a6a1259d0ff17b5256292e947
root <decourttthomas@gmail.com>
2025-06-17 20:06:12 +0000

    Add task 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+printf("%d \n", x);
+	
+y = x % 10;
+printf("The last digit of the number %d is %d \n", x, y);
+
+return (0);
+}
