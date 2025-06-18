ceb9c03bdda24bc4ffd6ac2d58b98d79317f2190
root <decourttthomas@gmail.com>
2025-06-18 13:00:12 +0000

    Add Task 4 lol

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - Prints a text according number
+* Return: Always (Success)     
+*/
+int main(void)
+{
+
+  char letters = 'a';
+  while (letters = 'z'; letters <= 'z'; letters++)
+    putchar(letters);
+    if letters == 'e' or letters == 'q':
+       continue
+	 putchar(letters);
+
+
+
+}
