9b53b78e8ddcd70bf7c9789cec9096204e623634
root <decourttthomas@gmail.com>
2025-06-18 12:24:57 +0000

    add task 3 test

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-/**                                                                                    
-* main - Prints a text according number                                                
-* Return: Always (Success)                                                             
+/**
+* main - Prints a text according number
+* Return: Always (Success)
 */
 int main(void)
 {
 char x;
-for(x = 'a'; x <= 'z'; x++)
+for (x = 'a'; x <= 'z'; x++)
 putchar(x);
-for(x = 'A'; x <= 'Z'; x++)
+for (x = 'A'; x <= 'Z'; x++)
 putchar(x);
 putchar('\n');
-return(0);
+return (0);
 }
