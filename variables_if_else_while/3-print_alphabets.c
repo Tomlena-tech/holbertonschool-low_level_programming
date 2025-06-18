356ff7902350cf04e1b3dd9c0c5fa20030947455
root <decourttthomas@gmail.com>
2025-06-18 11:19:07 +0000

    add task 2

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+ile Edit Options Buffers Tools C Help                                                 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**                                                                                    
+* main - Prints a text according number                                                
+* Return: Always (Success)                                                             
+*/
+int main()
+{
+  for(int x = 'a'; x <= 'z'; x++)
+    putchar(x);
+  
+  return(0);
+}
