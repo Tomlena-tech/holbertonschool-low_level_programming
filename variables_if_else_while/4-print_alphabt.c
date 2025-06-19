60aee756f62c61fa883dd1059e579157eb143738
root <decourttthomas@gmail.com>
2025-06-19 07:49:18 +0000

    Add task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -3,16 +3,16 @@
 #include <ctype.h>
 /**
 * main - Prints a text according number
-* Return: Always (Success)     
+* Return: Always (Success)
 */
 int main(void)
 {
 char letters = 'a';
 for (letters = 'a'; letters <= 'z'; letters++)
-  {
+{
 if (letters != 'e' && letters != 'q')
- putchar(letters);
-  }
- putchar('\n');
- return (0);
+putchar(letters);
+}
+putchar('\n');
+return (0);
 }
