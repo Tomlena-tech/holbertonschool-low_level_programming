c04a290f94014624fa3270380cebc33ce0d18b17
root <decourttthomas@gmail.com>
2025-06-19 11:13:57 +0000

    Add task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-/** * main - Prints letters
+/** main - Prints letters
 * Return: Always0(Success)
 */
 int main(void)
 {
-  int numbers; char letters;
-for (numbers = 0; numbers <=9; numbers ++)
+int numbers;
+char letters;
+for (numbers = 0; numbers <= 9; numbers++)
 {
 putchar(numbers + '0');
 }
- for (letters = 'a'; letters <= 'f'; letters ++)
+for (letters = 'a'; letters <= 'f'; letters++)
 {
 putchar(letters);
 }
