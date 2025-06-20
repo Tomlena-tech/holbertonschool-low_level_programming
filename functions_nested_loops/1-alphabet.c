7aeddd6c21d8e456a0323a3eaaa439dd199c75da
root <decourttthomas@gmail.com>
2025-06-20 09:46:43 +0000

    add task + maj main.h

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,9 +2,8 @@
 /**
 * main - check the code
 * Return: Always 0.
-* print_alphabet gonna print letters.
-*/
-void print_alphabet(void)
+* cette fonction imprime les lettres de l'alphabet dans l'ordre .*/
+void print_alphabet(void) 
 {char letter = 'a';
 while (letter <= 'z')
 {_putchar(letter);
