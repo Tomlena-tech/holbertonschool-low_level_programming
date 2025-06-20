3812bb9ec17b42b50aa6a05fea32d973ee8bd42a
root <decourttthomas@gmail.com>
2025-06-20 07:46:12 +0000

    Add main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,4 +2,9 @@
 // in a header file. Ideally there should be only
 // function declarations. Purpose of this code is
 // to only demonstrate working of header files.
-int _putchar(char c);
+#ifndef _putchar   /* Include guard */
+#define _putchar
+
+int _putchar(char c);  /* An example function declaration */
+
+#endif // FOO_H_
