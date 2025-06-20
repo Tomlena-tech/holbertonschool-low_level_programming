4b292f192881fbcd526404ccf233dc286344ca54
root <decourttthomas@gmail.com>
2025-06-20 08:17:08 +0000

    Add main.h le bon ?

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,9 @@
 #include "main.h"
-/* affichage des caracteres */
-/*main va servir a afficher*/
-int main(void)/* describ the main*/
+/**
+* main -  prints _putchar, followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 _putchar('_');
 _putchar('p');
