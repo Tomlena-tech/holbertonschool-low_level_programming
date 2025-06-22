07a80537b28059182f00bf630a0ed9d3f3bb5981
root <decourttthomas@gmail.com>
2025-06-22 12:13:32 +0000

    Add task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+int i;
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+{
+printf("%d, ", i);
+}
+printf("98\n");
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+printf("%d, ", i);
+}
+printf("98\n");
+}
+}
