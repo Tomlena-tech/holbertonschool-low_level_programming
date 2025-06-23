73d0e776a05127c46f0ecb8b76e16a21b2037dde
root <decourttthomas@gmail.com>
2025-06-23 10:14:21 +0000

    Add task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _isupper - checking uppercase letters
-* @c : character to check
-* return: 1 if it's uppercase, 0 it's lowercase.
+* _isupper - verifie si un caractere est une lettre majuscaule
+* @c: caractere (int code ASCII) 
+* description : teste sin c'est compris entre 'A' (65) et 'Z' (90), inclus
+* return : 1 si c'est majuscule, 0 sinon.
 */
 int _isupper(int c)
 {
