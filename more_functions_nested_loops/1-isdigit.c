f366b58c38bdeffdfbb0f0fe11c2c9511e6d5b25
root <decourttthomas@gmail.com>
2025-06-23 11:00:59 +0000

    task 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-/**                   
+/**
 * _isdigit - verifie si un caractere est un chiffre
 * @c: chiffre (int code ASCII)
 * Description : teste si l entier est un chiffre ou pas (48) et (57)
 * Return: 1 si c'est un chiffre, 0 non.                 
 */
-
 int _isdigit(int c)
 {
 if (c >= 48 && c < 58)
