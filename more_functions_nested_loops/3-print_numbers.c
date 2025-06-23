adfc33db36ff8d25d9679c7fe1570d604b52c231
root <decourttthomas@gmail.com>
2025-06-23 13:07:30 +0000

    Add task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *
- *
- *
- *
+ * print_numbers - affiche les chiffres.
+ *@c : variable chiffre
+ * Affiche les chiffres
+ * Return 
  */
-
 void print_numbers(void)
 {
 char c;
-for (c = 0; c < 9; c++)
+for (c = '0'; c <= '9'; c++)
 {_putchar (c);
 _putchar ('\n');
-}
+ }
 }
