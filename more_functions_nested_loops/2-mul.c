7cc1eb9d0abf199332fe9c2ad005eae44d95a9d0
root <decourttthomas@gmail.com>
2025-06-23 11:39:49 +0000

    Add task 2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
 * mul multiplie 2 entiers
-* @a@b: entiers
+* @a: nombre entiers 
+* @b: nombre entiers
 * Description  multiplication de a et b
 * Return: a * b resultat
 */
