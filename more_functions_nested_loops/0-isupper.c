b4fb764f35a38170103225d552dbd6f75f4d7c03
root <decourttthomas@gmail.com>
2025-06-23 09:56:17 +0000

    Add task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _isupper check capital letters or not
- * @c is the letter to check
+ * _isupper -  uppercase letters 
+ * @c :character to check
  * return 0 or 1
  */
 int _isupper(int c)
