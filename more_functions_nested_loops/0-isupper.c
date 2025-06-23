97e393f2ea2b20238beccbbb9d7519838c6243db
root <decourttthomas@gmail.com>
2025-06-23 09:57:57 +0000

    Add task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _isupper -  uppercase letters 
- * @c :character to check
- * return 0 or 1
- */
+ * _isupper - uppercase letters
+* @c :character to check
+* return 0 or 1
+*/
 int _isupper(int c)
 {
 if (c >= 65 && c < 91)
