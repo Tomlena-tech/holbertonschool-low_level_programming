e23be050ef7ec0a7274b3acb39c25c2110ad104a
root <decourttthomas@gmail.com>
2025-06-24 10:09:57 +0000

    Add task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * - print_most_numbers - print numbers except selected
+ *
+ *
+ *
+ * no return
+ */
+
+void print_most_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+if ((n != 2) && (n != 4))
+{
+_putchar(n + '0');
+}
+}      
+_putchar('\n');
+}
+
+
+
+
