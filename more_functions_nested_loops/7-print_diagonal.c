4c9093b8de29e57f7ffc5d2afb7dca79473ad700
root <decourttthomas@gmail.com>
2025-06-24 13:53:42 +0000

    Add task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
- *
- *
- *
+ *print_diagonal - prints a diagonal
+ *@n: parameter
+ *Return:returns nothing
  *
  *
  */
 
 void print_diagonal(int n)
 { int ligne;
-  int espace;
-  
-  if (n > 0)
-    {
-      for (ligne = 0; ligne < n; ligne++)
-	 {
-	   for (espace = 0; espace < ligne; espace++)
-	       _putchar (' ');
-		 _putchar ('\\');
-		 if (ligne == (n - 1))
-		   continue;
-		 _putchar ('\n');
-	       }
-	 }
+int espace;  
+if (n > 0)
+{
+for (ligne = 0; ligne < n; ligne++)
+{
+for (espace = 0; espace < ligne; espace++)
+_putchar (' ');
+_putchar ('\\');
+if (ligne == (n - 1))
+continue;
 _putchar ('\n');
-    }
+}
+}
+_putchar ('\n');
+}
 
