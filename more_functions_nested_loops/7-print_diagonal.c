1c2ce7d38e110e551129fa9207ac80d879826711
root <decourttthomas@gmail.com>
2025-06-24 13:50:11 +0000

    Add task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_diagonal(int n)
+{ int ligne;
+  int espace;
+  
+  if (n > 0)
+    {
+      for (ligne = 0; ligne < n; ligne++)
+	 {
+	   for (espace = 0; espace < ligne; espace++)
+	       _putchar (' ');
+		 _putchar ('\\');
+		 if (ligne == (n - 1))
+		   continue;
+		 _putchar ('\n');
+	       }
+	 }
+_putchar ('\n');
+    }
+
