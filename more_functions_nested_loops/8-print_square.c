4244e20a315fcfdbdd91729252a17bf01449e4cf
root <decourttthomas@gmail.com>
2025-06-25 07:37:54 +0000

    add task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,8 +4,27 @@
  *
  *
  *
- *
+ * Return (void)
  */
 
 void print_square(int size)
-  
+{
+int lon;
+int larj;
+
+
+
+  if (size > 0)
+    {
+      for (lon = 0; lon < size; lon++)
+      {
+	for (larj = 0; larj < size; larj++)
+	  {_putchar ('#');
+	   
+	    
+    }
+	            _putchar ('\n');
+	_putchar ('\n');
+      }
+    }
+}
