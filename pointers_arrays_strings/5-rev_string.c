0aefe372ff63ce014c8dad5b862430f39c2a8639
root <decourttthomas@gmail.com>
2025-06-27 12:59:39 +0000

    Add task 5 difficult

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string reverse string
+ *@s pointer to the string that will be reversed
+ *
+ *Return: (void)
+ */
+void rev_string(char *s)
+{
+int i;
+int len = 0;
+char tmp;
+while (s[len] != '\0') len++;
+len--;
+while (i < len)
+{
+tmp = s[i];
+s[i] = s[len];
+s[len] = tmp;
+i++;
+len--;
+}
+}
