a3c022fa32a9b42d7e70b6be1ccb277340fdd491
root <decourttthomas@gmail.com>
2025-06-27 09:50:06 +0000

    Add task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * _puts - is used to print string with automatic (\n) 
+ *@str: string to print in stdout
  *
- *
- *
- *
+ *Return: (void)
  *
  */
-
-void _puts(char *str);
+void _puts(char *str)
+{
+while (*str)
+_putchar(*str++);
+_putchar ('\n');
+}
