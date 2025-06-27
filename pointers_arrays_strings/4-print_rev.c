3cfe8a2c2246711f5006c33a6e264f57fd209188
root <decourttthomas@gmail.com>
2025-06-27 10:44:14 +0000

    Add task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev print string in reverse side
+ *@s: pointer to the string to orint in reverse
+ *
+ *
+ *Return(void)
+ */
+void print_rev(char *s)
+{
+int len = 0;
+int inde;
+ while (s[len] != '\0') len++; 
+for (inde = len - 1; inde >= 0; inde--)
+_putchar (s[inde]);
+
+_putchar ('\n');
+return;
+}
