197dfddeb26ffb35b2113664bc7783a36b55d8c3
root <decourttthomas@gmail.com>
2025-06-28 21:34:38 +0000

    Add task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_array - print n element of array
+*@a: array
+*@n: number of elements
+*Return: nothing
+*/
+void print_array(int *a, int n)
+{
+int compt_bcle ;
+for (compt_bcle = 0; compt_bcle < n ; compt_bcle++)
+{
+if (compt_bcle != n - 1)
+printf("%d, ", a[compt_bcle]);
+else
+printf("%d", a[compt_bcle]);
+
+}
+printf("\n");
+}
