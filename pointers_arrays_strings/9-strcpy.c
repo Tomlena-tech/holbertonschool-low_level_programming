d4cc00ffd3793db3ff8713c39544cc59fb8c5abc
root <decourttthomas@gmail.com>
2025-06-29 07:40:43 +0000

    Add task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strcpy - copying string into a buffer including null byte
+ *
+ *@dest buffer 
+ *@src string to copy
+ *Return : retourne at the beginning
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+char *_retourne = dest;
+while ((*dest++ = *src++) != '\0');
+return _retourne;
+}
+
