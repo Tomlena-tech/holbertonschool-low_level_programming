1a12b2fe8b46aceee2c701260b1d8bf465dfed18
root <decourttthomas@gmail.com>
2025-06-30 08:34:06 +0000

    Add task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <string.h>
+
 /**
 *  _strncpy - C function that copies a string, including the
 *  terminating null byte, using at most an inputted number of bytes.
@@ -12,10 +14,11 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
-for ( ; i < n; i++)
-dest[i] = '\0';
+int compteur_de_boucle;
+for (compteur_de_boucle = 0; compteur_de_boucle < n && src[compteur_de_boucle] != '\0'; compteur_de_boucle++)
+dest[compteur_de_boucle] = src[compteur_de_boucle];
+dest[compteur_de_boucle] = src[compteur_de_boucle];
+for ( ; compteur_de_boucle < n; compteur_de_boucle++)
+dest[compteur_de_boucle] = '\0';
 return (dest);
 }
