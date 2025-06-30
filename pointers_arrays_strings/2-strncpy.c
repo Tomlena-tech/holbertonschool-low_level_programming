f95da48f68b5cd10250c56050548f9eed9c15c89
root <decourttthomas@gmail.com>
2025-06-30 08:54:14 +0000

    add task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*   - C function that copies a string, including the
+*  _strncpy - C function that copies a string, including the
 *  terminating null byte, using at most an inputted number of bytes.
 *  If the length of the source string is less than the maximum byte number,
 *  the remainder of the destination string is filled with null bytes.
-*  Works identically to the standard library function `strncpy`.
+*  Works identically to the standard library function limited-copy function
 *@dest: buffer storing the string copy
 *@src:the source string
 *@n:max nummber of byte copied
