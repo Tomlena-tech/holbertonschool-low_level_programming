770752eaa014346d666baf568bdcaa07e00e0f1e
root <decourttthomas@gmail.com>
2025-07-02 08:39:36 +0000

    Add task 8 dure dure

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
 *print_diagsums -  a function that prints the sum of
@@ -7,8 +8,8 @@
 */
 void print_diagsums(int *a, int size)
 {
-int i, sum1 = 0, sum2 = 0;
- for (i = 0; i < size; i++)
+int i = 0, sum1 = 0, sum2 = 0;
+ for ( ; i < size; i++)
    {
      sum1 += *(a + i * size + i);
      sum2 += * (a + i * size + (size - 1 - i));
@@ -16,3 +17,4 @@ int i, sum1 = 0, sum2 = 0;
    }
 
  printf("%d, %d\n", sum1, sum2);
+}
