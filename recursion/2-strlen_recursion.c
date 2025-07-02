8acd074813355cbe774f90a319872989ae6f0395
root <decourttthomas@gmail.com>
2025-07-02 13:19:26 +0000

    add task 2 for Betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,22 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strlen_recursion count the length recursively 
+ *@s: string to calculate
+ * Return: length of the string
  */
-
-
-
-
-
 int _strlen_recursion(char *s)
 {
-  if (*s == '\0')
-    return(0);
-  return 1 + _strlen_recursion(s + 1);
-
-
-
+if (*s == '\0')
+return(0);
+return 1 + _strlen_recursion(s + 1);
 }
