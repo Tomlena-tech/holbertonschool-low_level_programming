8ff935a187580ccdd16de80915bc4fa245de1e23
root <decourttthomas@gmail.com>
2025-07-03 13:52:02 +0000

    Add task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,7 +3,28 @@
 * is_prime_number - returns true if the number is prime
 *@n: the number to check
 *
-*Return: true if the number is prime
+*Return: 1 if the number is prime
 */
+int check_prime(int n, int i);
+
+
+
 int is_prime_number(int n)
 {
+if (n <= 1)
+return (0);
+ if (n == 2)
+   return (1);
+return (check_prime(n, 2));
+}
+
+
+
+int check_prime(int n, int i)
+{
+if (i * i > n)
+return (1);
+if (n% i == 0)
+return (0);
+return (check_prime(n, i + 1));
+}
