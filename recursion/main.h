2ef93872a9a17008a2579dbd59aab8ec5ce47b53
root <decourttthomas@gmail.com>
2025-07-03 11:35:35 +0000

    Add task 5

--- recursion/main.h
+++ recursion/main.h
@@ -55,6 +55,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int x);
 #endif
 
 
