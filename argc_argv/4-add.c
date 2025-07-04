47ceabdd7ef421fcada1431cc3a10a0c625aee1f
root <decourttthomas@gmail.com>
2025-07-04 12:13:27 +0000

    Add task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,33 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
+ * - main add only positive number
+ *@argc: number of argumnets
+ *@argv array of argument strings
  *
- *
- *
- *
- *
+ *return: 0 if success, 1 if error
  **/
-
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
-
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-
-		sum += atoi(argv[i]);
-	}
-
-	printf("%d\n", sum);
-
-	return (0);
+int i, j, sum = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
 }
