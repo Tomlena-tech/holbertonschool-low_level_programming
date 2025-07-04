3a0899ddce2499bf500c381327edf423fd5f264d
root <decourttthomas@gmail.com>
2025-07-04 09:35:18 +0000

    Add task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**                                                                                                             
-* main - prints program name                                                                                    
+* main - prints number of arguments                                                                                    
 *@argc: number of arguments                                                                                     
 *@argv: array of arguments                                                                                      
 *Return: 0                                                                                                      
@@ -11,4 +11,3 @@ int main(int argc, char *argv[])
 printf("%d\n", argc - 1);
 return (0);
 }
-
