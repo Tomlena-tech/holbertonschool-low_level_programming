bcdcc35beb1a98a47e7e3cbe188c4977831706c1
root <decourttthomas@gmail.com>
2025-07-04 11:42:03 +0000

    Add task 4

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,13 +14,13 @@ int i = 0;
 int j = 0;  
 if (argc <= 2)
 {
-  printf("Error\n");
+printf("Error\n");
 return (1);
-}   
+}
 i = atoi(argv[1]);
 j = atoi(argv[2]);
 result = i * j;
 printf("%d\n", result);
-return (0); 
+return (0);
 }
 
