dca10246af5c9a40b2ba2e9353b620a294e3812d
Tomlena-tech <decourttthomas@gmail.com>
2025-07-08 10:58:29 +0000

    exercice 3 suite

--- malloc_free/ 3-alloc_grid.c
+++ malloc_free/ 3-alloc_grid.c
@@ -9,3 +9,31 @@
  * Return: pointer to the 2 dimensional array, or NULL on failure
  */
 int **alloc_grid(int width, int height)
+{
+int **grid; int index1, index2;
+
+    if (width <= 0 || height <= 0)
+    {
+        return NULL;
+    }
+   
+   grid = malloc(sizeof(int *) * height);
+        if (grid == NULL)
+        {
+            return NULL;
+        }
+    for (index1 = 0; index1 < height; index1++)
+    {
+        grid[index1] = malloc(sizeof(int) * width);
+        if (grid[index1] == NULL)
+        {return (NULL);
+        }
+            for (index2 = 0; index2 < width; index2++)
+            {grid[index1][index2] = 0;
+            }
+            free(grid);
+            return NULL;
+
+        }
+    return grid;
+}
