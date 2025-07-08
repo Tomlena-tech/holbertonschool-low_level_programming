9d4ab7bc0bb40d34828bff3a7ac81c55c1335bc8
Tomlena-tech <decourttthomas@gmail.com>
2025-07-08 09:39:11 +0000

    debut du 3

---
+++ malloc_free/ 3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: pointer to the 2 dimensional array, or NULL on failure
+ */
+int **alloc_grid(int width, int height)
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -60,4 +60,5 @@ int main(void);/* modifie pour l'exo 0 a la base argc argv */
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 #endif /* MAIN_H */
