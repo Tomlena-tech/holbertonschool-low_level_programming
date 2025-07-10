2130c74882ce4850edb04a5ff5e6c047cc28757d
Tomlena-tech <decourttthomas@gmail.com>
2025-07-10 12:25:58 +0200

    Add task 0

--- structutres_typedef/dog.h
+++ structutres_typedef/dog.h
@@ -1,20 +1,22 @@
-#ifndef DOG_H   /* Include guard */
+#ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - A structure to represent a dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
- struct dog
- {
- char *name; 
- float age;     
- char *owner;   
- };
- /**
-  * dog_t - Typedef for struct dog.
-  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 #endif /* DOG_H */
