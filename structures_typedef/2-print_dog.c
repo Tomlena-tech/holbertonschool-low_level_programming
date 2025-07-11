9c8d85ef2f7624811add78aa3a1f4535e73143fc
Tomlena-tech <decourttthomas@gmail.com>
2025-07-11 11:38:25 +0200

    add task 2 finall

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include "dog.h"
-/**
- * 
+/** print_dog - prints a struct dog
+ * @d pointer to the struct doog to print
+ *  Description: This function prints the name, age, and owner of a dog.
+ * if any firlds are missing  it prints (nil)
  */
 void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
   if (d->name == NULL && d->owner == NULL)
-    {
-        printf("Name: (nil)\n");
-        printf("Age: %f\n", d->age);
-        printf("Owner: (nil)\n");
-    }
-    else if (d->name == NULL)
-    {
-        printf("Name: (nil)\n");
-        printf("Age: %f\n", d->age);
-        printf("Owner: %s\n", d->owner);
-    }
-    else if (d->owner == NULL)
-    {
-        printf("Name: %s\n", d->name);
-        printf("Age: %f\n", d->age);
-        printf("Owner: (nil)\n");
-    }
-    else
-    {
-        printf("Name: %s\n", d->name);
-        printf("Age: %f\n", d->age);
-        printf("Owner: %s\n", d->owner);
-    }
+{ 
+printf("Name: (nil)\n");
+printf("Age: %f\n", d->age);
+printf("Owner: (nil)\n");
+}
+else if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
+else if (d->owner == NULL)
+{
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
 }
