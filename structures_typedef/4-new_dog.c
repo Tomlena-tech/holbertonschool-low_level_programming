4ea7b31c4be097d944d867ac081b6bc3e7c7eb33
Tomlena-tech <decourttthomas@gmail.com>
2025-07-11 12:48:44 +0200

    add task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,32 +11,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+
     dog_t *dog;
-    int name_len = 0, owner_len = 0;
+    int name_len, owner_len;
 
     if (name == NULL || owner == NULL)
         return (NULL);
-
-    while (name[name_len])
-        name_len++;
-    while (owner[owner_len])
-        owner_len++;
 
     dog = malloc(sizeof(dog_t));
     if (dog == NULL)
         return (NULL);
 
-    dog->name = malloc(name_len + 1);
-    if (dog->name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
+    for (name_len = 0; name[name_len]; name_len++)
+        ;
+    for (owner_len = 0; owner[owner_len]; owner_len++)
+        ;
 
+    dog->name = malloc(name_len + 1);
     dog->owner = malloc(owner_len + 1);
-    if (dog->owner == NULL)
+    if (dog->name == NULL || dog->owner == NULL)
     {
         free(dog->name);
+        free(dog->owner);
         free(dog);
         return (NULL);
     }
