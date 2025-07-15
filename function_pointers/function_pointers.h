79a6010e9ec5adb20641ae03a31b9e8235001267
Tomlena-tech <decourttthomas@gmail.com>
2025-07-15 13:02:03 +0200

    Add task 2

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -17,6 +17,7 @@ void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 typedef void (*func_ptr)(char *);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 /**
  * int_index - Searches for an integer in an array.
  * @array: The array to search.
