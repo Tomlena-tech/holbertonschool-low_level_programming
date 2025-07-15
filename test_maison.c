71525abd51ffb141cb5bda6af4810cc22fdb3f7f
Tomlena-tech <decourttthomas@gmail.com>
2025-07-15 09:06:23 +0200

    essai d exo maison

---
+++ test_maison.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() 
+
+{ int nombrtableau[5] = {0};
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        printf("la valeur a l'indice 3 est de %d\n", nombrtableau[3]);
+  
+    return 0;
+    }
+
+ 
+
+}   
