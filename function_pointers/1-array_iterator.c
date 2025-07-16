16933015457e0ea0e1314c3707eeb72a513c1790
Tomlena-tech <decourttthomas@gmail.com>
2025-07-16 09:25:40 +0200

    correction task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,11 +11,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;           
-    if (array == NULL || action == NULL || size == 0)
-        return;     
-    for (i = 0; i < size; i++)
-    {
-        action(array[i]);  // Call the function pointed to by action
-    }
+size_t i;           
+if (array == NULL || action == NULL || size == 0)
+return;     
+for (i = 0; i < size; i++)
+{
+action(array[i]);  
+}
 }
