b653579c6bd80a79cb208fcc974881976ddd8340
Tomlena-tech <decourttthomas@gmail.com>
2025-07-28 15:55:32 +0200

    Add task 2 beggining

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -20,9 +20,9 @@ typedef struct listint_s
 } list_t;
 
 size_t print_list(const list_t *h);
-list_t *add_nodeint(list_t **head, const int n);
 void free_listint(list_t *head);
 int check_cycle(list_t *list);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
