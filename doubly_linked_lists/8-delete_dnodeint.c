7cd89d053a30a6fc38ef7c7d0098d073da1b5794
Tomlena-tech <decourttthomas@gmail.com>
2025-07-31 14:45:36 +0200

    Add beginnig task 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
